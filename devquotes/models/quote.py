66bb4a1c98408c7e8e8f6f87532d49687b4a6170
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-07 12:19:11 +0800

    Created Quote model

---
+++ devquotes/models/quote.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from . import db
+
+utcnow = datetime.utcnow
+
+
+class Quote(db.Model):
+    __tablename__ = 'quote'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100), nullable=False)
+    quotation = db.Column(db.String(200), nullable=False)
+    source = db.Column(db.String, nullable=True)
+    likes = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, default=utcnow)
+    updated_at = db.Column(db.DateTime, default=utcnow, onupdate=utcnow)
