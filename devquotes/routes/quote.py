c8adb196eba1b75a2851c0ff0bc15d29b96d4536
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-07 14:26:52 +0800

    Route to retrieve paginated quotes

--- devquotes/routes/quote.py
+++ devquotes/routes/quote.py
@@ -4,22 +4,33 @@ from flask_restful import (
     Resource,
 )
 
-from .db_client import create_quote
-from .fields import quote_fields
+from . import db_client
+from .fields import (
+    quote_fields,
+    quotes_fields
+)
 
 
 class QuoteList(Resource):
 
     def __init__(self):
         self.parser = reqparse.RequestParser()
+        self.parser.add_argument('page', type=int, location='args')
+        self.parser.add_argument('per_page', type=int, location='args')
+
         self.parser.add_argument('author', type=str, required=True)
         self.parser.add_argument('quotation', type=str, required=True)
         self.parser.add_argument('source', type=str, required=False)
 
+    @marshal_with(quotes_fields)
+    def get(self):
+        args = self.parser.parse_args()
+        return db_client.get_quotes(args['page'], args['per_page'])
+
     @marshal_with(quote_fields)
     def post(self):
         args = self.parser.parse_args()
-        return create_quote({
+        return db_client.create_quote({
             'author': args['author'],
             'quotation':  args['quotation'],
             'source': args['source'].strip() if args['source'].strip() else None
