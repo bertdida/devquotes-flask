191661f0d19fbf756506c20d31447c190206f7f9
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-07 12:18:43 +0800

    Delete shell context function, import Quote model

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 import os
 
 from devquotes import create_app
-
-from devquotes.models.user import User
+from devquotes.models.user import User  # pylint: disable=unused-import
+from devquotes.models.quote import Quote  # pylint: disable=unused-import
 
 app = create_app(config_class=os.environ['CONFIG_CLASS'])
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'User': User}
-
 
 if __name__ == '__main__':
     app.run()
