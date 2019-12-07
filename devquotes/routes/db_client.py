37c894481cef6b7530fd21acd3e5a07c155b6ccc
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-07 14:25:57 +0800

    Added function to retrieve quotes

--- devquotes/routes/db_client.py
+++ devquotes/routes/db_client.py
@@ -1,5 +1,14 @@
+from flask import current_app
+
 from devquotes.models.user import User
 from devquotes.models.quote import Quote
+
+
+def _paginate(query, page, per_page):
+    if per_page is None:
+        per_page = current_app.config.get('QUOTES_PER_PAGE', 10)
+
+    return query.paginate(page, per_page, error_out=True)
 
 
 def get_user(firebase_user_id):
@@ -12,3 +21,8 @@ def create_user(data):
 
 def create_quote(data):
     return Quote.create(**data)
+
+
+def get_quotes(page, per_page):
+    query = Quote.query.order_by(Quote.created_at.desc())
+    return _paginate(query, page, per_page)
