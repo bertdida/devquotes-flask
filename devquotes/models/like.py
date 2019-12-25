7916e91a8e00e3a5b7610ba501cc89b261f9a629
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-25 13:17:30 +0800

    Added like table

---
+++ devquotes/models/like.py
@@ -0,0 +1,10 @@
+from . import db
+from .mixins import BaseMixin
+
+
+class Like(BaseMixin, db.Model):
+    __tablename__ = 'like'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    quote_id = db.Column(db.Integer, db.ForeignKey('quote.id'), nullable=False)
