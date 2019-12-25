dbfacf23ef1ea225dba499c3b857ce70d17578a3
bertdida <38489033+bertdida@users.noreply.github.com>
2019-12-25 13:17:56 +0800

    Import Like model

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 from devquotes import create_app
 from devquotes.models.user import User  # pylint: disable=unused-import
 from devquotes.models.quote import Quote  # pylint: disable=unused-import
+from devquotes.models.like import Like  # pylint: disable=unused-import
 
 app = create_app(config_class=os.environ['CONFIG_CLASS'])
 
