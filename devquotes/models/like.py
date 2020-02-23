76aed68860514221b3d2aa97a325a2bc55e53da0
bertdida <38489033+bertdida@users.noreply.github.com>
2020-02-23 17:25:35 +0800

    Added created_at and updated_at on like table

--- devquotes/models/like.py
+++ devquotes/models/like.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from . import db
 from .mixins import BaseMixin
+
+utcnow = datetime.utcnow
 
 
 class Like(BaseMixin, db.Model):
@@ -8,3 +12,5 @@ class Like(BaseMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     quote_id = db.Column(db.Integer, db.ForeignKey('quote.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=utcnow)
+    updated_at = db.Column(db.DateTime, default=utcnow, onupdate=utcnow)
