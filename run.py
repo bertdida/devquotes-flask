d6d901e70f92247e33e230837eb4b37c584564cb
bertdida <38489033+bertdida@users.noreply.github.com>
2020-02-29 13:54:01 +0800

    Added make_shell_context function in run.py

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 import os
 
 from devquotes import create_app
+from devquotes.models import db
 from devquotes.models.user import User  # pylint: disable=unused-import
 from devquotes.models.quote import Quote  # pylint: disable=unused-import
 from devquotes.models.like import Like  # pylint: disable=unused-import
 
 app = create_app(config_class=os.environ['CONFIG_CLASS'])
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db}
+
 
 if __name__ == '__main__':
     app.run()
