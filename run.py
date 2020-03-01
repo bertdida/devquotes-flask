3e1714b6d0aa0ecc065bcfeab4bcddb5f975db28
bertdida <38489033+bertdida@users.noreply.github.com>
2020-03-01 21:47:37 +0800

    Delete make_shell_context, added routes and seed command

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import json
+import operator
 import os
 
 from devquotes import create_app
@@ -9,9 +11,31 @@ from devquotes.models.like import Like  # pylint: disable=unused-import
 app = create_app(config_class=os.environ['CONFIG_CLASS'])
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db}
+@app.before_first_request
+def create_database_tables():
+    db.create_all()
+
+
+@app.cli.command()
+def routes():
+    rules = []
+    for rule in app.url_map.iter_rules():
+        methods = ','.join(sorted(rule.methods))
+        rules.append((rule.endpoint, methods, str(rule)))
+
+    sort_by_rule = operator.itemgetter(2)
+    for endpoint, methods, rule in sorted(rules, key=sort_by_rule):
+        route = '{:50s} {:25s} {}'.format(endpoint, methods, rule)
+        print(route)
+
+
+@app.cli.command()
+def seed():
+    with open('quotes.json') as json_file:
+        quotes = json.load(json_file)
+        quote_objects = [Quote(**data) for data in quotes]
+        db.session.bulk_save_objects(quote_objects)
+        db.session.commit()
 
 
 if __name__ == '__main__':
