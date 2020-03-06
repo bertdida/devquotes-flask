1b2b8f9e167a43586cd32480baad9cba814c763b
bertdida <38489033+bertdida@users.noreply.github.com>
2020-03-06 22:12:17 +0800

    Changed firebase_user_id type to string

--- devquotes/models/user.py
+++ devquotes/models/user.py
@@ -6,5 +6,5 @@ class User(BaseMixin, db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    firebase_user_id = db.Column(db.Integer, nullable=False, unique=True)
+    firebase_user_id = db.Column(db.String, nullable=False, unique=True)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
