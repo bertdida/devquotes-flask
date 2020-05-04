4f9d39182fba269c5054e696d8788122b9678b96
bertdida <38489033+bertdida@users.noreply.github.com>
2020-05-04 22:16:12 +0800

    Initialize elasticsearch if ELASTICSEARCH_URL config is set

--- devquotes/__init__.py
+++ devquotes/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from elasticsearch import Elasticsearch
 
 
 def create_app(config_class):
     app = Flask(__name__)
     app.config.from_object(config_class)
     app.url_map.strict_slashes = False
+
+    app.es = None
+    if app.config['ELASTICSEARCH_URL']:
+        app.es = Elasticsearch([app.config['ELASTICSEARCH_URL']])
 
     from devquotes import models, routes
     models.init_app(app)
