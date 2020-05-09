c6304cbb66d0027ea602fc6f3e60b493319aaecc
bertdida <38489033+bertdida@users.noreply.github.com>
2020-05-09 23:00:13 +0800

    Initialize elasticsearch for production to use AWS service

--- devquotes/__init__.py
+++ devquotes/__init__.py
@@ -1,5 +1,9 @@
+from elasticsearch import (
+    Elasticsearch,
+    RequestsHttpConnection
+)
 from flask import Flask
-from elasticsearch import Elasticsearch
+from requests_aws4auth import AWS4Auth
 
 
 def create_app(config_class):
@@ -8,8 +12,25 @@ def create_app(config_class):
     app.url_map.strict_slashes = False
 
     app.es = None
+
     if app.config['ELASTICSEARCH_URL']:
-        app.es = Elasticsearch([app.config['ELASTICSEARCH_URL']])
+        params = {'hosts': app.config['ELASTICSEARCH_URL']}
+        if app.config['ENV'] == 'production':
+            awsauth = AWS4Auth(
+                app.config['AWS_ACCESS_KEY'],
+                app.config['AWS_SECRET_KEY'],
+                app.config['AWS_REGION'],
+                'es'  # AWS service
+            )
+
+            params.update({
+                'http_auth': awsauth,
+                'use_ssl': True,
+                'verify_certs': True,
+                'connection_class': RequestsHttpConnection
+            })
+
+        app.es = Elasticsearch(**params)
 
     from devquotes import models, routes
     models.init_app(app)
