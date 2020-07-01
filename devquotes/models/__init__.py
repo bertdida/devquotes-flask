f750b81406fd67ce6e479354ae8a1a13ad61b55b
bertdida <38489033+bertdida@users.noreply.github.com>
2020-07-01 21:35:57 +0800

    Call sqlalchemy-searchable make_searchable function

--- devquotes/models/__init__.py
+++ devquotes/models/__init__.py
@@ -1,7 +1,11 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy_searchable import make_searchable
+
 db = SQLAlchemy()
+make_searchable(db.metadata)
+
 migrate = Migrate(compare_type=True)
 
 
