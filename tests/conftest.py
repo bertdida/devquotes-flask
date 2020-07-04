a9ff65415a0912f14112bd45b9d07c187cdb7213
bertdida <38489033+bertdida@users.noreply.github.com>
2020-07-04 15:16:16 +0800

    Use testing.postgresql library, added database fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from contextlib import contextmanager
 
-from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token,
-)
+from flask_jwt_extended import create_access_token, create_refresh_token
+import testing.postgresql as postgresql
 
+import config
 from devquotes import create_app
 from devquotes.models import db
 from devquotes.models.quote import Quote
@@ -13,6 +12,49 @@ from devquotes.models.user import User
 
 MOCK_FIREBASE_USER_ID = 'firebaseuserid'
 MOCK_FIREBASE_JWT = 'firebasejwt'
+
+
+@pytest.fixture(name='firebase_user_id', scope='session')
+def setup_and_teardown_user_id():
+    return MOCK_FIREBASE_USER_ID
+
+
+@pytest.fixture(name='firebase_jwt', scope='session')
+def setup_and_teardown_jwt():
+    return MOCK_FIREBASE_JWT
+
+
+@pytest.fixture(name='postgres', scope='session')
+def setup_and_teardown_postgres():
+    Postgresql = postgresql.PostgresqlFactory(cache_initialized_db=True)
+
+    postgres = Postgresql()
+    yield postgres
+
+    postgres.stop()
+    Postgresql.clear_cache()
+
+
+@pytest.fixture(name='app', scope='session')
+def setup_and_teardown_app(postgres):
+    class TestingConfig(config.Testing):
+        SQLALCHEMY_DATABASE_URI = postgres.url()
+
+    return create_app(TestingConfig)
+
+
+@pytest.fixture(name='database', scope='module', autouse=True)
+def setup_and_teardown_database(app):
+    with app.app_context():
+        db.create_all()
+
+    yield db
+    db.drop_all()
+
+
+@pytest.fixture(name='client', scope='session')
+def setup_and_teardown_client(app):
+    return app.test_client()
 
 
 @contextmanager
@@ -24,22 +66,6 @@ def db_session_no_expire():
         yield
     finally:
         session.expire_on_commit = True
-
-
-@pytest.fixture(name='app', scope='package')
-def setup_and_teardown_app():
-    app = create_app('config.Testing')
-
-    with app.app_context():
-        db.create_all()
-
-    yield app
-    db.drop_all()
-
-
-@pytest.fixture(name='client', scope='module')
-def setup_and_teardown_client(app):
-    return app.test_client()
 
 
 @pytest.fixture(name='quote', scope='module', autouse=True)
@@ -57,11 +83,6 @@ def setup_and_teardown_quote(app):
     quote.delete()
 
 
-@pytest.fixture(name='firebase_user_id', scope='module')
-def setup_and_teardown_firebase_user_id():
-    return MOCK_FIREBASE_USER_ID
-
-
 @pytest.fixture(name='admin_user', scope='module', autouse=True)
 def setup_and_teardown_firebase_admin_user(app, firebase_user_id):
     data = {
@@ -76,13 +97,8 @@ def setup_and_teardown_firebase_admin_user(app, firebase_user_id):
     user.delete()
 
 
-@pytest.fixture(name='firebase_jwt')
-def setup_and_teardown_firebase_jwt():
-    return MOCK_FIREBASE_JWT
-
-
 @pytest.fixture(name='client_admin', scope='module')
-def setup_and_teardown_client_admin(app, client, admin_user):
+def setup_and_teardown_client_admin(client, admin_user):
     identity = {
         'firebase_user_id': admin_user.firebase_user_id,
         'is_admin': admin_user.is_admin,
