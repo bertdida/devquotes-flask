ceaa9ea6ba51070e1799779699fbe5fbb7d924e6
bertdida <38489033+bertdida@users.noreply.github.com>
2020-07-21 19:49:02 +0800

    Make users/user_id route accessible by admin only

--- devquotes/routes/user.py
+++ devquotes/routes/user.py
@@ -1,14 +1,15 @@
-from flask_restful import (
-    marshal_with,
-    Resource,
-)
+from flask_jwt_extended import jwt_required
+from flask_restful import marshal_with, Resource
 
 from . import db_client
 from .fields import user_fields
+from .utils import admin_only
 
 
 class User(Resource):
 
     @marshal_with(user_fields)
+    @jwt_required
+    @admin_only
     def get(self, user_id):
         return db_client.get_user_or_404(user_id)
