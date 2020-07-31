dc204e245589ef2d358295f3d989d5aaa00f3178
bertdida <38489033+bertdida@users.noreply.github.com>
2020-07-31 13:53:34 +0800

    Changes on like model - remove id, created_at and updated_at columns - remove unique constraint for user_id and quote_id - make user_id and quote_id primary

--- devquotes/models/like.py
+++ devquotes/models/like.py
@@ -1,19 +1,13 @@
-from datetime import datetime
 
 from . import db
 from .mixins import BaseMixin
-
-utcnow = datetime.utcnow
 
 
 class Like(BaseMixin, db.Model):
     __tablename__ = 'like'
     __table_args__ = (
-        db.UniqueConstraint('user_id', 'quote_id', name='uc_like'),
+        db.PrimaryKeyConstraint('user_id', 'quote_id'),
     )
 
-    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     quote_id = db.Column(db.Integer, db.ForeignKey('quote.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=utcnow)
-    updated_at = db.Column(db.DateTime, default=utcnow, onupdate=utcnow)
