88aa4a63a2bddd507196d5d5a363a7647d3bd9d7
bertdida <38489033+bertdida@users.noreply.github.com>
2020-08-02 15:13:01 +0800

    Added /users/me route

--- devquotes/routes/__init__.py
+++ devquotes/routes/__init__.py
@@ -28,7 +28,7 @@ def init_app(app):
     from .auth import Token, TokenRevoke, TokenRefresh
     from .quote import Quotes, Quote, Random as RandomQuote, Contributor
     from .likes import Likes, Like
-    from .user import User
+    from .user import User, CurrentUser
 
     api.init_app(bp)
     api.add_resource(Token, '/auth/token')
@@ -40,6 +40,7 @@ def init_app(app):
     api.add_resource(Likes, '/likes')
     api.add_resource(Like, '/likes/<int:quote_id>')
     api.add_resource(User, '/users/<int:user_id>')
+    api.add_resource(CurrentUser, '/users/me')
     api.add_resource(Contributor, '/quotes/<int:quote_id>/contributor')
 
     app.register_blueprint(bp, url_prefix='/v1')
