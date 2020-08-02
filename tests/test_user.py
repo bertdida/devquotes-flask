dc8dc55676500c1f69967676e85d2f60c7647867
bertdida <38489033+bertdida@users.noreply.github.com>
2020-08-02 14:09:35 +0800

    Added tests for getting user

---
+++ tests/test_user.py
@@ -0,0 +1,13 @@
+from .utils.assertions import assert_valid_schema
+
+
+def test_get_user(client, user_admin):
+    resp = client.get('/v1/users/{0.id}'.format(user_admin))
+    assert resp.status_code == 401
+
+
+def test_admin_get_user(client_admin, user_admin):
+    resp = client_admin.get('/v1/users/{0.id}'.format(user_admin))
+
+    assert resp.status_code == 200
+    assert_valid_schema(resp.json, 'user.json')
