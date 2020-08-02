69ab5865d724218170e618d42fd600b6133c6b68
bertdida <38489033+bertdida@users.noreply.github.com>
2020-08-02 15:14:14 +0800

    Added test_unauthorized_get_me and test_authorized_get_me tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,3 +11,15 @@ def test_admin_get_user(client_admin, user_admin):
 
     assert resp.status_code == 200
     assert_valid_schema(resp.json, 'user.json')
+
+
+def test_unauthorized_get_me(client):
+    resp = client.get('/v1/users/me')
+    assert resp.status_code == 401
+
+
+def test_authorized_get_me(client_admin):
+    resp = client_admin.get('/v1/users/me')
+
+    assert resp.status_code == 200
+    assert_valid_schema(resp.json, 'user.json')
