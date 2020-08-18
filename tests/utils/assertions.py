c56422bc905348584dc0b4a927681c100faddef9
bertdida <38489033+bertdida@users.noreply.github.com>
2020-08-18 18:08:17 +0800

    Use f string

--- tests/utils/assertions.py
+++ tests/utils/assertions.py
@@ -24,7 +24,7 @@ def _load_json_schema(filename):
     absolute_path = join(dirname(__file__), relative_path)
 
     base_path = dirname(absolute_path)
-    base_uri = 'file:///{}/'.format(base_path)
+    base_uri = f'file:///{base_path}/'
 
     with open(absolute_path) as schema_file:
         return jsonref.loads(schema_file.read(), base_uri=base_uri, jsonschema=True)
